273e9289ea734f9101618bbbbe8d48d67b70977e
FelipeMDantas <felipemdantas@terra.com.br>
2022-10-24 22:44:51 -0300

    Api data retrieval and global state management set.

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
 import { genres } from "../assets/constants";
-import { SongCard } from "../components";
+import { Error, Loader, SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = "Pop";
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
@@ -19,9 +27,16 @@ const Discover = () => {
         </select>
       </div>
 
-      <div className="flex flex-wrap sm:justify-start sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={i}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+          />
         ))}
       </div>
     </div>
